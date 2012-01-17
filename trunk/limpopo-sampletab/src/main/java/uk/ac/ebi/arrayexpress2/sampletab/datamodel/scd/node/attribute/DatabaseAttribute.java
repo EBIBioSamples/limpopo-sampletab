30c4244a36bdb08c0024c5ac853b0ea9a09e1f2b
afaulconbridge <afaulconbridge@1ea05776-3933-4300-8799-de4109d95848>
2012-01-17 11:25:28 +0000

    Added additional constructor

--- trunk/limpopo-sampletab/src/main/java/uk/ac/ebi/arrayexpress2/sampletab/datamodel/scd/node/attribute/DatabaseAttribute.java
+++ trunk/limpopo-sampletab/src/main/java/uk/ac/ebi/arrayexpress2/sampletab/datamodel/scd/node/attribute/DatabaseAttribute.java
@@ -4,36 +4,47 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DatabaseAttribute extends AbstractNodeAttribute {
-	public String databaseID;
-	public String databaseURI;
+    public String databaseID;
+    public String databaseURI;
 
-	public String getAttributeType() {
-		return "Database Name";
-	}
+    public DatabaseAttribute() {
+        super();
+    }
 
-	public String[] headers() {
-		List<String> headersList = new ArrayList<String>();
-		headersList.add(getAttributeType());
-		if (databaseID != null) {
-			headersList.add("Database ID");
-		}
-		if (databaseURI != null) {
-			headersList.add("Database URI");
-		}
-		String[] result = new String[headersList.size()];
-		return headersList.toArray(result);
-	}
+    public DatabaseAttribute(String name, String id, String uri) {
+        this();
+        this.setAttributeValue(name);
+        this.databaseID = id;
+        this.databaseURI = uri;
+    }
 
-	public String[] values() {
-		List<String> valuesList = new ArrayList<String>();
-		valuesList.add(getAttributeValue());
-		if (databaseID != null) {
-			valuesList.add(databaseID);
-		}
-		if (databaseURI != null) {
-			valuesList.add(databaseURI);
-		}
-		String[] result = new String[valuesList.size()];
-		return valuesList.toArray(result);
-	}
+    public String getAttributeType() {
+        return "Database Name";
+    }
+
+    public String[] headers() {
+        List<String> headersList = new ArrayList<String>();
+        headersList.add(getAttributeType());
+        if (databaseID != null) {
+            headersList.add("Database ID");
+        }
+        if (databaseURI != null) {
+            headersList.add("Database URI");
+        }
+        String[] result = new String[headersList.size()];
+        return headersList.toArray(result);
+    }
+
+    public String[] values() {
+        List<String> valuesList = new ArrayList<String>();
+        valuesList.add(getAttributeValue());
+        if (databaseID != null) {
+            valuesList.add(databaseID);
+        }
+        if (databaseURI != null) {
+            valuesList.add(databaseURI);
+        }
+        String[] result = new String[valuesList.size()];
+        return valuesList.toArray(result);
+    }
 }
