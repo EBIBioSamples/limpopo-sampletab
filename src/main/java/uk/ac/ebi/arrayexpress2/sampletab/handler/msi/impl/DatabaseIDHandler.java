e7efb5c5c9da9c32dfe83f907bd9abeed61fba58
Adam Faulconbridge <afaulconbridge@users.noreply.github.com>
2012-03-07 10:22:07 +0000

    Temporary partial revert to enable compile.

--- src/main/java/uk/ac/ebi/arrayexpress2/sampletab/handler/msi/impl/DatabaseIDHandler.java
+++ src/main/java/uk/ac/ebi/arrayexpress2/sampletab/handler/msi/impl/DatabaseIDHandler.java
@@ -2,6 +2,7 @@ package uk.ac.ebi.arrayexpress2.sampletab.handler.msi.impl;
 
 import net.sourceforge.fluxion.spi.ServiceProvider;
 import uk.ac.ebi.arrayexpress2.magetab.exception.ParseException;
+import uk.ac.ebi.arrayexpress2.magetab.handler.listener.HandlerEvent;
 import uk.ac.ebi.arrayexpress2.sampletab.datamodel.MSI;
 import uk.ac.ebi.arrayexpress2.sampletab.handler.msi.MSIReadHandler;
 
@@ -10,6 +11,9 @@ public class DatabaseIDHandler extends MSIReadHandler {
     @Override
     public void read(String header, String[] data, MSI msi, int lineNumber, int columnNumber) throws ParseException {
     	//TODO finish
+        fireHandlingStartedEvent(new HandlerEvent(this, HandlerEvent.Type.READ, data));
+        fireHandlingSucceededEvent(new HandlerEvent(this, HandlerEvent.Type.READ, data));
+        //fireHandlingFailedEvent(new HandlerEvent(this, HandlerEvent.Type.READ, data));
     }
 
     public boolean canReadHeader(String header) {
