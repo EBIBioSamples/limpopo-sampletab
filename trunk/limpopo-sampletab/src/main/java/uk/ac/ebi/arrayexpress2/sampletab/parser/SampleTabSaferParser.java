7d39b2d80088c0af3ac961982e85dbb9d2f427e1
afaulconbridge <afaulconbridge@1ea05776-3933-4300-8799-de4109d95848>
2012-06-28 13:23:03 +0000

    Added validation constructor

--- trunk/limpopo-sampletab/src/main/java/uk/ac/ebi/arrayexpress2/sampletab/parser/SampleTabSaferParser.java
+++ trunk/limpopo-sampletab/src/main/java/uk/ac/ebi/arrayexpress2/sampletab/parser/SampleTabSaferParser.java
@@ -1,10 +1,6 @@
 package uk.ac.ebi.arrayexpress2.sampletab.parser;
 
-import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
@@ -17,52 +13,29 @@ import org.slf4j.LoggerFactory;
 
 import uk.ac.ebi.arrayexpress2.magetab.exception.ParseException;
 import uk.ac.ebi.arrayexpress2.magetab.listener.ErrorItemListener;
+import uk.ac.ebi.arrayexpress2.magetab.validator.Validator;
 import uk.ac.ebi.arrayexpress2.sampletab.datamodel.SampleData;
 
 public class SampleTabSaferParser extends SampleTabParser<SampleData> {
-    private final SampleTabParser<SampleData> parser = new SampleTabParser<SampleData>();
     private final List<ErrorItem> errorItems = new ArrayList<ErrorItem>();
 
     public Logger log = LoggerFactory.getLogger(getClass());
 
     public SampleTabSaferParser() {
+        this(null);
+    }
+	
+	public SampleTabSaferParser(Validator<SampleData> validator) {
+        super(validator);
         //make a listener to put error items in the list
-        parser.addErrorItemListener(new ErrorItemListener() {
+        super.addErrorItemListener(new ErrorItemListener() {
             public void errorOccurred(ErrorItem item) {
                 errorItems.add(item);
             }
         });
-        
-    }
-    
-    public SampleData parse(File msiFile) throws ParseException {
-        try {
-            return parse(msiFile.toURI().toURL());
-        }
-        catch (MalformedURLException e) {
-            throw new ParseException("File '" + msiFile.getAbsolutePath() + " could not be resolved to a valid URL", e);
-        }
     }
 
-    public SampleData parse(URL msiURL) throws ParseException {
-        try {
-            return parse(msiURL.openStream());
-        }
-        catch (IOException e) {
-            throw new ParseException("Could not open a connection to " + msiURL.toString(), e);
-        }
-    }
-    
-    public SampleData parse(String filename) throws ParseException {
-        log.debug("Starting parsing "+filename+"...");
-    	return parse(new File(filename));
-    }
-
-	public SampleData parse(InputStream dataIn) throws ParseException {
-        return parse(dataIn, new SampleData());
-	}
-	
-	public synchronized SampleData parse(InputStream dataIn, SampleData target)
+    public synchronized SampleData parse(InputStream dataIn, SampleData target)
 			throws ParseException {      
         
 
@@ -70,7 +43,7 @@ public class SampleTabSaferParser extends SampleTabParser<SampleData> {
         ExecutorService service = Executors.newSingleThreadExecutor();
         
         try { 
-            sd = parser.parse(dataIn, target, service);
+            sd = super.parse(dataIn, target, service);
             if (errorItems.size() > 0){
                 throw new ParseException(true, errorItems.size()+" error items detected", errorItems.toArray(new ErrorItem[]{}));
             }
