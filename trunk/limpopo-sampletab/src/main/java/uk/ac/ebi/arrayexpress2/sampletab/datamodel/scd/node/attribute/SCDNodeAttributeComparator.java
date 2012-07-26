ca8f16b70ae99a1c85e4adde088ad2c23fc5d22e
afaulconbridge <afaulconbridge@1ea05776-3933-4300-8799-de4109d95848>
2012-07-26 12:25:10 +0000

    Improved handling of duplicate attributes

---
+++ trunk/limpopo-sampletab/src/main/java/uk/ac/ebi/arrayexpress2/sampletab/datamodel/scd/node/attribute/SCDNodeAttributeComparator.java
@@ -0,0 +1,69 @@
+package uk.ac.ebi.arrayexpress2.sampletab.datamodel.scd.node.attribute;
+
+public class SCDNodeAttributeComparator implements java.util.Comparator<SCDNodeAttribute> {
+
+    public int compare(SCDNodeAttribute arg0, SCDNodeAttribute arg1) {
+        //handle nulls
+        if (arg0 == null){
+            if (arg1 == null){
+                return 0;
+            } else { 
+                return 1;
+            }
+        } else if (arg1 == null){
+            return -1;
+        }
+        
+        if (arg0.getAttributeType().equals(arg1.getAttributeType())){
+            if (arg0.getAttributeValue().equals(arg1.getAttributeValue())){
+                //at his point, one or both of them may be ontology terms.
+                //cast and behave accordingly
+                
+                boolean hasOntology = false;
+                synchronized(AbstractNodeAttributeOntology.class){
+                    if (AbstractNodeAttributeOntology.class.isInstance(arg0) && AbstractNodeAttributeOntology.class.isInstance(arg0)){
+                        hasOntology = true;
+                    }
+                }
+                
+                if (hasOntology){
+                    AbstractNodeAttributeOntology ont0 = (AbstractNodeAttributeOntology) arg0;
+                    AbstractNodeAttributeOntology ont1 = (AbstractNodeAttributeOntology) arg1;
+                
+                    if (ont0.getTermSourceREF() == null && ont1.getTermSourceREF() == null) {
+                        return 0;
+                    } else if (ont0.getTermSourceREF() == null && ont1.getTermSourceREF() != null) {
+                        return 1;
+                    } else if (ont0.getTermSourceREF() != null && ont1.getTermSourceREF() == null) {
+                        return -1;
+                    }  else if (ont0.getTermSourceREF().equals(ont1.getTermSourceREF())) {
+                        //fall back to id
+                        if (ont0.getTermSourceID() == null && ont1.getTermSourceID() == null) {
+                            return 0;
+                        } else if (ont0.getTermSourceID() == null && ont1.getTermSourceID() != null) {
+                            return 1;
+                        } else if (ont0.getTermSourceID() != null && ont1.getTermSourceID() == null) {
+                            return -1;
+                        }  else if (ont0.getTermSourceID().equals(ont1.getTermSourceID())) {
+                            return 0;
+                            //new things to check go here...
+                        } else {
+                            return ont0.getTermSourceID().compareTo(ont1.getTermSourceID());
+                        }
+                    } else {
+                        return ont0.getTermSourceREF().compareTo(ont1.getTermSourceREF());
+                    }
+                    
+                } else {
+                    return 0;
+                }
+            } else {
+                return arg0.getAttributeValue().compareTo(arg1.getAttributeValue());
+            }
+        } else {
+            return arg0.getAttributeType().compareTo(arg1.getAttributeType());
+        }
+        
+    }
+
+}
