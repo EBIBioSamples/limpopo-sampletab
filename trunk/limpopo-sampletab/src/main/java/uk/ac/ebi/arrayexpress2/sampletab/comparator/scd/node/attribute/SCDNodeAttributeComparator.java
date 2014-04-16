373a6f01f8951c8a019c0d8aa77877a9800fcf12
afaulconbridge <afaulconbridge@1ea05776-3933-4300-8799-de4109d95848>
2014-04-16 11:16:54 +0000

    Re written scdnodecomparator to be based on comparison by column headers and values

--- trunk/limpopo-sampletab/src/main/java/uk/ac/ebi/arrayexpress2/sampletab/comparator/scd/node/attribute/SCDNodeAttributeComparator.java
+++ trunk/limpopo-sampletab/src/main/java/uk/ac/ebi/arrayexpress2/sampletab/comparator/scd/node/attribute/SCDNodeAttributeComparator.java
@@ -1,6 +1,7 @@
 package uk.ac.ebi.arrayexpress2.sampletab.comparator.scd.node.attribute;
 
 import uk.ac.ebi.arrayexpress2.sampletab.datamodel.scd.node.attribute.AbstractNodeAttributeOntology;
+import uk.ac.ebi.arrayexpress2.sampletab.datamodel.scd.node.attribute.DatabaseAttribute;
 import uk.ac.ebi.arrayexpress2.sampletab.datamodel.scd.node.attribute.SCDNodeAttribute;
 
 public class SCDNodeAttributeComparator implements java.util.Comparator<SCDNodeAttribute> {
@@ -17,56 +18,23 @@ public class SCDNodeAttributeComparator implements java.util.Comparator<SCDNodeA
             return -1;
         }
         
-        if (arg0.getAttributeType().equals(arg1.getAttributeType())) {
-            if (arg0.getAttributeValue().equals(arg1.getAttributeValue())) {
-                //at his point, one or both of them may be ontology terms.
-                //cast and behave accordingly
-                
-                boolean hasOntology = false;
-                synchronized(AbstractNodeAttributeOntology.class) {
-                    if (AbstractNodeAttributeOntology.class.isInstance(arg0) && AbstractNodeAttributeOntology.class.isInstance(arg0)) {
-                        hasOntology = true;
-                    }
-                }
-                
-                if (hasOntology) {
-                    AbstractNodeAttributeOntology ont0 = (AbstractNodeAttributeOntology) arg0;
-                    AbstractNodeAttributeOntology ont1 = (AbstractNodeAttributeOntology) arg1;
-                
-                    if (ont0.getTermSourceREF() == null && ont1.getTermSourceREF() == null) {
-                        return 0;
-                    } else if (ont0.getTermSourceREF() == null && ont1.getTermSourceREF() != null) {
-                        return 1;
-                    } else if (ont0.getTermSourceREF() != null && ont1.getTermSourceREF() == null) {
-                        return -1;
-                    }  else if (ont0.getTermSourceREF().equals(ont1.getTermSourceREF())) {
-                        //fall back to id
-                        if (ont0.getTermSourceID() == null && ont1.getTermSourceID() == null) {
-                            return 0;
-                        } else if (ont0.getTermSourceID() == null && ont1.getTermSourceID() != null) {
-                            return 1;
-                        } else if (ont0.getTermSourceID() != null && ont1.getTermSourceID() == null) {
-                            return -1;
-                        }  else if (ont0.getTermSourceID().equals(ont1.getTermSourceID())) {
-                            return 0;
-                            //new things to check go here...
-                        } else {
-                            return ont0.getTermSourceID().compareTo(ont1.getTermSourceID());
-                        }
-                    } else {
-                        return ont0.getTermSourceREF().compareTo(ont1.getTermSourceREF());
-                    }
-                    
-                } else {
-                    return 0;
-                }
-            } else {
-                return arg0.getAttributeValue().compareTo(arg1.getAttributeValue());
-            }
+        //compare based on the headers and values of the attribute
+        if (arg0.headers().length < arg0.headers().length) {
+            return -1;
+        } else if (arg0.headers().length > arg0.headers().length) {
+            return 1;
         } else {
-            return arg0.getAttributeType().compareTo(arg1.getAttributeType());
+            for (int i = 0; i < arg0.headers().length; i++) {
+                int j = arg0.headers()[i].compareTo(arg1.headers()[i]);
+                if (j != 0) return j;
+            }
+            for (int i = 0; i < arg0.values().length; i++) {
+                int j = arg0.values()[i].compareTo(arg1.values()[i]);
+                if (j != 0) return j;
+            }
+            return 0;
         }
-        
+
     }
 
 }
