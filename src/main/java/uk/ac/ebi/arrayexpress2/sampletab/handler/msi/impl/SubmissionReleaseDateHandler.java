9fbbf8088c46fb6c234863081c4931e4145dfc2e
Adam Faulconbridge <afaulconbridge@users.noreply.github.com>
2014-05-15 15:21:09 +0000

    Reduced logging of date formating variants

--- src/main/java/uk/ac/ebi/arrayexpress2/sampletab/handler/msi/impl/SubmissionReleaseDateHandler.java
+++ src/main/java/uk/ac/ebi/arrayexpress2/sampletab/handler/msi/impl/SubmissionReleaseDateHandler.java
@@ -2,6 +2,9 @@ package uk.ac.ebi.arrayexpress2.sampletab.handler.msi.impl;
 
 import java.text.SimpleDateFormat;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import net.sourceforge.fluxion.spi.ServiceProvider;
 import uk.ac.ebi.arrayexpress2.magetab.exception.ParseException;
 import uk.ac.ebi.arrayexpress2.sampletab.datamodel.MSI;
@@ -9,6 +12,9 @@ import uk.ac.ebi.arrayexpress2.sampletab.handler.msi.MSIReadHandler;
 
 @ServiceProvider
 public class SubmissionReleaseDateHandler extends MSIReadHandler {
+    
+    private Logger log = LoggerFactory.getLogger(getClass());
+    
     @Override
     public int getAllowedLength() {
         return 1;
@@ -16,38 +22,23 @@ public class SubmissionReleaseDateHandler extends MSIReadHandler {
 
     @Override
     protected void readValue(MSI msi, String value, int lineNumber, String... types) throws ParseException  {
-		boolean flag = false;
 		String[] date_formats = { "yyyy-MM-dd", "yyyy/MM/dd", "dd/MM/yyyy",
 				"dd-MM-yyyy", "yyyy MMM dd", "yyyy dd MMM", "dd MMM yyyy",
 				"yyyyMMdd", "ddMMyyyy", "yyyy\\MM\\dd", "dd\\MM\\yyyy" };
+		
 		for (String formatdate : date_formats) {
 			try {
-				synchronized (SubmissionUpdateDateHandler.simpledateformat) {
-					msi.submissionReleaseDate = new SimpleDateFormat(formatdate)
-							.parse(value);
-					System.out.println("the date format is" + formatdate);
-					flag = true;
-					break;
-				}
-			} catch (Exception e) {
-				System.out.println("The date format is not " + formatdate);
-				continue;
-			}
+				msi.submissionReleaseDate = new SimpleDateFormat(
+                        formatdate).parse(value);
+                log.trace("Submission Release Date format is " + formatdate);
+                break;
+            } catch (java.text.ParseException e) {
+                log.trace("Submission Release Date format is not " + formatdate);
+                continue;
+            }
 		}
 
-	}
-    /**   if (!value.matches("[0-9]{2,4}/[0-9]{1,2}/[0-9]{1,2}"))
-            throw new ParseException("Invalid SubmissionReleaseDate \""+value+"\"");
-        
-    	try {
-    	    synchronized (SubmissionUpdateDateHandler.simpledateformat){
-                msi.submissionReleaseDate = SubmissionUpdateDateHandler.simpledateformat.parse(value);
-    	    }
-		} catch (java.text.ParseException e) {
-            throw new ParseException(e);
-		}
-    }*/
-    
+	}    
 
     public boolean canReadHeader(String header) {
         return header.equals("submissionreleasedate");
--- src/main/java/uk/ac/ebi/arrayexpress2/sampletab/handler/msi/impl/SubmissionUpdateDateHandler.java
+++ src/main/java/uk/ac/ebi/arrayexpress2/sampletab/handler/msi/impl/SubmissionUpdateDateHandler.java
@@ -3,6 +3,9 @@ package uk.ac.ebi.arrayexpress2.sampletab.handler.msi.impl;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import net.sourceforge.fluxion.spi.ServiceProvider;
 import uk.ac.ebi.arrayexpress2.magetab.exception.ParseException;
 import uk.ac.ebi.arrayexpress2.sampletab.datamodel.MSI;
@@ -10,7 +13,9 @@ import uk.ac.ebi.arrayexpress2.sampletab.handler.msi.MSIReadHandler;
 
 @ServiceProvider
 public class SubmissionUpdateDateHandler extends MSIReadHandler {
-    public static SimpleDateFormat simpledateformat = new SimpleDateFormat("yyyy/MM/dd");
+    
+    private Logger log = LoggerFactory.getLogger(getClass());
+    
     @Override
     public int getAllowedLength() {
         return 1;
@@ -18,39 +23,22 @@ public class SubmissionUpdateDateHandler extends MSIReadHandler {
 
     @Override
     protected void readValue(MSI msi, String value, int lineNumber, String... types) throws ParseException  {
-		boolean flag = false;
-
 		String[] date_formats = { "yyyy-MM-dd", "yyyy/MM/dd", "dd/MM/yyyy", "yyyy\\MM\\dd", "dd\\MM\\yyyy",
 				"dd-MM-yyyy", "yyyy MMM dd", "yyyy dd MMM", "dd MMM yyyy",
 				"yyyyMMdd", "ddMMyyyy" };
 
 		for (String formatdate : date_formats) {
 			try {
-				synchronized (simpledateformat) {
-					msi.submissionUpdateDate = new SimpleDateFormat(
-							formatdate).parse(value);
-					System.out.println("the date format is" + formatdate);
-					flag = true;
-					break;
-				}
-			} catch (Exception e) {
-				System.out.println("The date format is not " + formatdate);
+				msi.submissionUpdateDate = new SimpleDateFormat(
+						formatdate).parse(value);
+                log.trace("Submission Update Date format is " + formatdate);
+				break;
+            } catch (java.text.ParseException e) {
+				log.trace("Submission Update Date format is not " + formatdate);
 				continue;
-			}
+            }
 		}
 	}
-
-     /** if (!value.matches("[0-9]{2,4}/[0-9]{1,2}/[0-9]{1,2}"))
-           throw new ParseException("Invalid SubmissionUpdateDate \""+value+"\"");
-        
-    	try {
-            synchronized (simpledateformat){
-                msi.submissionUpdateDate = simpledateformat.parse(value);
-            }
-		} catch (java.text.ParseException e) {
-            throw new ParseException(e);
-		}
-    }*/
   
 
     public boolean canReadHeader(String header) {
